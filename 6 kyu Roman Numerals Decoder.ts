d34e727ea965e52623eea64dab6d612a1f1f30ef
rhlcoder <rhlcoder@gmail.com>
2020-02-19 13:07:50 -0300

    Update 6 kyu Roman Numerals Decoder.ts

--- 6 kyu Roman Numerals Decoder.ts
+++ 6 kyu Roman Numerals Decoder.ts
@@ -1,1 +1,38 @@
+// https://www.codewars.com/kata/51b6249c4612257ac0000005/javascript
+
 // https://www.codewars.com/kata/51b6249c4612257ac0000005/train/javascript
+
+/* Convertir estos valores a un obj {symbol:value}?
+    Symbol    Value
+    I          1
+    V          5
+    X          10
+    L          50
+    C          100
+    D          500
+    M          1000
+    IV         4
+    IX         9
+    XL         40
+    XC         90
+    CD         400
+    CM         900
+*/
+
+/* Combinaciones de a dos validas:
+	   I       V	   X	   L	   C	   D	   M
+I	   2       4	   9	   x	   x	   x	   x
+V	   6       x	   x	   x	   x	   x	   x
+X	  11      15	  20	  40	  90	   x	   x
+L	  51      55	  60	   x	   x	   x	   x
+C	 101     105	 110	 150	 200	 400	 900
+D	 501     505	 510	 550	 600	   x	   x
+M	1001	1005	1010	1050	1100	1500	2000
+*/
+
+  // complete the solution by transforming the 
+  // string roman numeral into an integer  
+
+function solution(roman: string): number {
+    return 0
+}
