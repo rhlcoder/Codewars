627b86ad3f497f616839fc8067fef2da7f291fa8
rhlcoder <rhlcoder@gmail.com>
2020-02-20 23:51:50 -0300

    Update 5 kyu Maximum subarray sum.ts

--- 5 kyu Maximum subarray sum.ts
+++ 5 kyu Maximum subarray sum.ts
@@ -1,10 +1,59 @@
 // https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/javascript
 
-/* The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+/* VERSION 1: intentare eliminar los for anidados */
 
-maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-// should be 6: [4, -1, 2, 1]
+function slicer(arr:number[], min:number, max:number) {
+  return arr.slice(min, max).reduce((a, b) => {
+    return a + b;
+  }, 0);
+}
+function iterate(arr:number[]) {
+  var res = [];
+  for (let min = 0; min < arr.length; min++) {
+    for (let max = min + 1; max <= arr.length; max++) {
+      res.push(slicer(arr, min, max));
+    }
+  }
+  return res;
+}
 
-Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+function maxSequence(list:number[]) {
+  return !list.length
+    ? 0
+    : iterate(list).reduce((acc, curr) => {
+        return acc > curr ? acc : curr;
+      }, 0);
+}
 
-Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray. */
+/* VERSION 2: sin For, pero varias chapuzas para hacer flatten (que no anda en codewars por requerir node 12 */
+/* Usa 2 maps y 3 reduces :( */
+
+function maxSequence1(list: number[]): number {
+  return !list.length
+    ? 0
+    : list
+        .map((_, start, a) => a.map((_, end, aa) => aa.slice(start, end + 1)
+        .reduce((a, b) => a + b, 0)))
+        .reduce((a, b) => a.concat(b))
+        .reduce((a, b) => (a > b ? a : b), 0);
+}
+
+
+/* Esta la hizo un genio de la internet con un solo reduce */
+function maxSequence(arr: number[]): number {
+  let currentSum = 0;
+  return arr.reduce((maxSum: number, number: number) => {
+    currentSum = Math.max(currentSum + number, 0);
+    return Math.max(currentSum, maxSum);
+  }, 0);
+}
+
+/* Datos de prueba */
+
+const f = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const g = [3, -2, 10, -6];
+const h = [-2, -1, -3, -4, -1, -5];
+
+console.log(maxSequence(f));
+console.log(maxSequence(g));
+console.log(maxSequence(h));
