b899ef0f865b62eb7bc8324e36771bcef109d3f0
rhlcoder <rhlcoder@gmail.com>
2020-02-23 02:10:03 -0300

    Update 6 kyu Decode the Morse code.ts

--- 6 kyu Decode the Morse code.ts
+++ 6 kyu Decode the Morse code.ts
@@ -9,14 +9,40 @@ const MORSE_LIST = {
 "-.--"      : "Y",  "--.."      : "Z",  "-----"     : "0",  ".----"     : "1",
 "..---"     : "2",  "...--"     : "3",  "....-"     : "4",  "....."     : "5",
 "-...."     : "6",  "--..."     : "7",  "---.."     : "8",  "----."     : "9",
-"...---..." : "SOS",  "_"       : " ",  "-.-.--"    : "!",  ".-.-.-"    : "."
+
+".-.-.-"    : ".",  "---..."    : ":",  "-.-.-."    : ";",  "..--.."    : "?",
+"-...-"     : "=",  ".----."    : "'",  "-..-."     : "/",  "-.-.--"    : "!",
+"-....-"    : "-",  "..--.-"    : "_",  "-.--."     : "(",  "-.--.-"    : ")",
+"...-..-"   : "$",  ". . . ."   : "&",  ".--.-."    : "@", //"--..--'  : ", ",
+
+"...---..." : "SOS",
 };
 
+
+// function decodeMorse(morseCode: string): string {
+//   return morseCode
+//     .trim()
+//     .replace(/(?<=(-|\.))\s(?=(-|\.))/g, 'x')
+//     .replace(/   /g,'x x')
+//     .split('x')
+//     .map(morse => MORSE_CODE[morse] || ' ')
+// }
+
+// Esta version no funciona en codewars porque "matchAll" pertenece a ES2020, pero funciona bien.
+
+/* exprimiendo el regex como un campeon:
+  [-.]+       --> Cualquier combinacion de codigo morse
+  (?<=[.-]\s) --> Al fin implementaron lookbehinds en JS! Indica que le precede un '. ' o un '- '
+  \s          --> un espacio
+  (?=\s[.-])  -->  Tambien uso un lookahead. Este indica que le sucede un ' .' o un ' -'
+  /g          --> para que busque en todo el string, sino devuelve solo el primer match
+*/
+const re = /[-.]+|(?<=[.-]\s)\s(?=\s[.-])/g 
+
 function decodeMorse(morseCode: string): string {
-  return morseCode
-    .trim()
-    .replace(/   /g, ' _ ')
-    .split(' ')
-    .map(morse => MORSE_LIST[morse])
+  return Array.from(
+    morseCode
+    .matchAll(re), m => m[0]) 
+    .map(morse => MORSE_CODE[morse] || ' ')
     .join('')
 }
