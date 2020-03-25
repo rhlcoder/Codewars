f8efbe97880ff7e622ec6561baa6dfc6fbd4d68b
rhlcoder <rhlcoder@gmail.com>
2020-03-24 21:09:02 -0300

    Create 8 kyu Will you make it.java

---
+++ 8 kyu Will you make it.java
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5861d28f124b35723e00005e/train/java
+
+public class Kata {
+  
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+    return distanceToPump <= mpg * fuelLeft;
+  }
+  
+}
